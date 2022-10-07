a30ba396ff055f1972d72f88632fcad6f78efc6e
Giuliano De Vito <giuliano_devito@hotmail.com>
2022-10-07 13:17:19 -0300

    Cambios login visuales

--- src/App.js
+++ src/App.js
@@ -14,13 +14,16 @@ function App() {
 
   return (
     <div className="App">
-      <h2>Black Gestion</h2>
-      <h1>Presupuestos</h1> 
+      <div className='contenedorTitular'>
+        <h2>Black Gestion</h2>
+        <h1>Presupuestos</h1> 
+      </div> 
+
 
       <Input inputTipe="email" placeHolder="Email"/>
       <Input inputTipe="password" placeHolder="Contraseña"/>
 
-      <div>
+      <div className='contenedorBtnSesion'>
         <Boton tipoBtn="inicioSesion" texto="Iniciar Sesion" functionClick={iniciarSesion}/>
         <Boton tipoBtn="nuevaCuenta" texto="Crear Usuario" functionClick={crearUsuario}/>
       </div>
